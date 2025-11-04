b87d372b98272de3c73cdcb25994b8605f295612
richalok <alokyoyo001tiwari@gmail.com>
2025-11-04 12:31:32 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    numbers= [10,20,30,40,50]
+    average= sum(numbers)/len(numbers)
+    print("Numbers:", numbers)
+    print("Average:", average)
+
+if __name__=="__main__":
+    main()
